df93a44ab9145e109ad4137659d6af89cf050c6f
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-05-21 18:14:25 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -41,7 +41,7 @@ const [best , setBests ] = useState(data);
       
       <div className='text-center items-center mx-auto my-5 flex flex-col gap-2 md:flex-row md:gap-10 mt-20'>
       <button className=' bg-purple-500 border-none rounded-sm text-white w-1/2 md:w-2/3'>Checkout</button>
-      <NavLink to='/Food_Delivery_App/' className=' bg-purple-500 border-none rounded-sm text-white w-1/2 md:w-2/3'>
+      <NavLink to='/' className=' bg-purple-500 border-none rounded-sm text-white w-1/2 md:w-2/3'>
       <button className='border-none'>Continue Discovering</button>
       </NavLink>
      </div>
